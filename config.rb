336ef29fefe8b51c9f7851b38e9c47ed42322836
Dominik Porada <dominik@porada.co>
2013-11-22 00:47:28 +0100

    :tractor:

--- config.rb
+++ config.rb
@@ -16,3 +16,35 @@ configure :build do
   # Use relative URLs
   activate :relative_assets
 end
+
+helpers do
+  def student_section?
+    current_page.path.start_with?('student')
+  end
+
+  def teacher_section?
+    current_page.path.start_with?('teacher')
+  end
+
+  def current_section
+    if student_section?
+      'student'
+    elsif teacher_section?
+      'teacher'
+    else
+      ''
+    end
+  end
+
+  def active?(*names)
+    if current_page.path.start_with?(*names)
+      'active'
+    else
+      ''
+    end
+  end
+
+  def icon(name)
+    "<span class=\"icon glyphicon glyphicon-#{name}\"></span>"
+  end
+end

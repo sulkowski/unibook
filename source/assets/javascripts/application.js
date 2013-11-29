aa75f72d422e5512857ac6d0a576827f12ae65a0
Mateusz Tuński <mateusz@tunski.net>
2013-11-29 10:52:05 +0100

    Upgrade messaging and login

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -7,3 +7,21 @@
 function go(url) {
   document.location.href = url;
 }
+
+function switchLang(elem) {
+  $("#pl").removeClass("active");
+  $("#en").removeClass("active");
+  $(elem).addClass("active");
+}
+
+
+
+$(function () {
+    $('#myModal3').on('hidden.bs.modal', function () {
+    go("./index.html");
+  })
+  $('#myModal2').on('hidden.bs.modal', function () {
+    go("../index.html");
+  })
+});
+
